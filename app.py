a6609d37c9fc8a32f8de15173c4f4cf07fd53c93
0xania <145127682+0xania@users.noreply.github.com>
2025-05-21 20:24:42 +0200

    Play audio directive

--- app.py
+++ app.py
@@ -6,24 +6,29 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def main():
     req = request.get_json()
+    audio_url = "https://drive.google.com/uc?export=download&id=1nIsjdQZX333HK7zlkn2NEoRfje8EOYgd"
 
-    res = {
+    response = {
+        "version": req.get("version", "1.0"),
+        "session": req.get("session"),
         "response": {
-            "text": "Начинаю тренировку. Включаю ваш аудиофайл.",
-            "tts": "Начинаю тренировку. Включаю ваш аудиофайл.",
-            "end_session": False,
-            "buttons": [
+            "text": "Начинаю тренировку. Воспроизводится аудио.",
+            "tts": "Начинаю тренировку. Воспроизводится аудио.",
+            "end_session": True,
+            "directives": [
                 {
-                    "title": "Слушать тренировку",
-                    "url": "https://drive.google.com/uc?export=download&id=1nIsjdQZX333HK7zlkn2NEoRfje8EOYgd",
-                    "hide": False
+                    "type": "AudioPlayer.Play",
+                    "audio_item": {
+                        "stream": {
+                            "url": audio_url,
+                            "offset_ms": 0
+                        }
+                    }
                 }
             ]
-        },
-        "version": req.get("version", "1.0")
+        }
     }
-
-    return jsonify(res)
+    return jsonify(response)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
