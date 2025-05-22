a64aa2c723d0843790a14a0cdd820cca2798aebc
0xania <145127682+0xania@users.noreply.github.com>
2025-05-22 20:48:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def main():
         "response": {
             "text": "Воспроизвожу тренировку",
             "tts": (
-                "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/7c639f4f-345d-4201-b486-bbf9f46f327e.opus\"/> "
+                "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/7a68b042-694a-4dc8-9658-d09068ec991d.opus\"/> "
+                "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/b94113bf-6344-428b-b9a1-174db2485fab.opus\"/> "
                 "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/0e4c93f1-019d-4c8f-9195-e0f709b6d51f.opus\"/> "
                 "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/a0acbef5-5562-49cc-966d-0f9bc35eba43.opus\"/> "
                 "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/ac484922-1130-45e1-9ede-a8a13d8c6e11.opus\"/>"
