7a48f236be58619f782b9fcb8fd5702b49edb75c
0xania <145127682+0xania@users.noreply.github.com>
2025-05-22 18:13:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,7 @@ def main():
     res = {
         "response": {
             "text": "",
-            "tts": (
-                "<speak>"
-                "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/7c639f4f-345d-4201-b486-bbf9f46f327e.opus\"/>"
-                "<break time=\"1s\"/>"
-                "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/0e4c93f1-019d-4c8f-9195-e0f709b6d51f.opus\"/>"
-                "<break time=\"1s\"/>"
-                "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/a0acbef5-5562-49cc-966d-0f9bc35eba43.opus\"/>"
-                "<break time=\"1s\"/>"
-                "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/ac484922-1130-45e1-9ede-a8a13d8c6e11.opus\"/>"
-                "</speak>"
-            ),
+            "tts": "<speaker audio=\"dialogs-upload/e8946e14-7a9f-4d28-b210-43e493267ff2/7c639f4f-345d-4201-b486-bbf9f46f327e.opus\"/>",
             "end_session": True
         },
         "version": req.get("version", "1.0")
